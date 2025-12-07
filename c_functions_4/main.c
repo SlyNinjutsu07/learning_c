1ac510aa4a95dc2b285e68a33acacd9ed3f7ab32
Sly-UbuntuWindowsTerminal <85970858+SlyNinjutsu07@users.noreply.github.com>
2025-12-07 13:16:39 -0500

    Just made a few changes; Making my way through chapter 4

--- c_functions_4/main.c
+++ c_functions_4/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
-int main(){
+int plus_one(int n);//Prototype or declaration
+
+char* hello(void);//MUST use void
+
+int main(void){
+
+	int i = 10;
+	printf("i + 1 : %d\n", plus_one(i));
+	printf("%s", hello());
 
 	return 0;
+}
+
+//Func definition
+int plus_one(int n){
+	return n + 1;
+}
+
+//Must use void to say no parameters, good practice
+char* hello(void){
+	return "Hello, World\n";
 }
--- c_one/hello.c
+++ c_one/hello.c
@@ -4,16 +4,22 @@
 
 int main(void){
 
+	// for(;;) condition = Inf loop
+	// if(1) or if(0) = if(true) or if(false)
+	// sizeof(val) returns a size_t
+	// size_t can be printed with %zu in printf
+
 	int x = 1;
 
 	switch(x){
 		case 1:
 			printf("1\n");
-			break;
+			break; //Remove break to have a "fall-through"
 		case 2:
 			printf("2\n");
 			break;
 	}
+	
 	
 	
     return 0;

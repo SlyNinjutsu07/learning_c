89cc5ebe15e3756a9e8e2550f83375d5231ac53c
Sly-UbuntuWindowsTerminal <85970858+SlyNinjutsu07@users.noreply.github.com>
2025-12-07 11:22:49 -0500

    Played around with some programs in hello.c; Following beej's book of C(beejbkmk); Add new dir c_functions_4 for chapter 4 - functions

--- c_one/hello.c
+++ c_one/hello.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 int main(void){
 
-    int i = 31;
-    float f = 3.14;
-    char* s = "Hello, World";
+	int x = 1;
 
-    printf("%d\n", i);
-    ++i;
-    printf("%d\n", i);
-    i++;
-    printf("%d\n", i);
-
+	switch(x){
+		case 1:
+			printf("1\n");
+			break;
+		case 2:
+			printf("2\n");
+			break;
+	}
+	
+	
     return 0;
 }

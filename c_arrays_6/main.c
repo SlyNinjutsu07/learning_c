d0972cb244e07e35b017a33c2fdb847d161c16ec
Sly-UbuntuWindowsTerminal <85970858+SlyNinjutsu07@users.noreply.github.com>
2025-12-09 11:53:24 -0500

    Forgot to commit these changes; Modified main.c in c_arrays; Up to Chapter 6.3

--- c_arrays_6/main.c
+++ c_arrays_6/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main(void){
+	
+	//Sugar candy for pointers n sh
+	int arr[5];//If given no values, it points
+			   //to random numbers in memory
 
+	int r[4];
+	r[0] = 1; r[1] = 2; r[2] = 3; r[3] = 4;
+	
+	for(int i = 0; i < 4; i++)
+		printf("%d : %d\n", i, r[i]);
+
+	//Getting size = sizeof(arr) / sizeof(arrType)
+	printf("%zu / %zu = %zu\n", sizeof(r), sizeof(int), sizeof(r) / sizeof(int));
+	printf("Int Arr 5 : %zu\n", sizeof arr);//Each added elem adds memory
+								//5 = 20 bytes
+	printf("Double Arr w/ 48 : %zu\n", sizeof( double [48] ));//A double is 8 bytes
 	return 0;
 }

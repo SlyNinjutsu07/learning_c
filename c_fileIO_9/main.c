36140685845b88c8c49849ae7fd7203539b0388e
Sly-UbuntuWindowsTerminal <85970858+SlyNinjutsu07@users.noreply.github.com>
2025-12-26 13:02:59 -0500

    Add hello.txt for notes

--- c_fileIO_9/main.c
+++ c_fileIO_9/main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
+void print_whole_line(char *fn, FILE *fp){
+		int c = 0;
+		fp = fopen(fn, "r");
+
+		while((c = fgetc(fp)) != EOF)
+				printf("%c", c);
+		fclose(fp);
+}
+
 int main(void){
 
-	return 0;
+
+		//FILE* data type
+		//stdin -> Standard Input -> default to keyboard
+		//stdout -> Stand. Output -> default to the screen
+		//stderr -> Stand. Error -> default to screen
+
+		//These two are the same thing
+		printf("Hello, world\n");
+		fprintf(stdout, "Hello, world\n");
+
+		FILE *fp; //Var represents an OPEN FILE
+		fp = fopen("hello.txt", "r"); //Open hello.txt for "r"eading
+		int c = fgetc(fp);
+		printf("%c\n", c);
+		fclose(fp);
+
+		print_whole_line("hello.txt", fp);
+
+		return 0;
 }
+
+

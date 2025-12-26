2a5a3a6c039829f74e3245ea5804bf3810063449
Sly-UbuntuWindowsTerminal <85970858+SlyNinjutsu07@users.noreply.github.com>
2025-12-26 15:14:33 -0500

    Modify help.txt; Add notes.md for notes

--- c_fileIO_9/main.c
+++ c_fileIO_9/main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
-void print_whole_line(char *fn, FILE *fp){
+void print_whole_file(char *fn, FILE *fp){
 		int c = 0;
 		fp = fopen(fn, "r");
 
 		while((c = fgetc(fp)) != EOF)
 				printf("%c", c);
 		fclose(fp);
+}
+
+void print_in_lines(void){
+		int lineN = 0;
+		FILE *f;
+		char s[1024]; //Allocate bytes for reading text
+
+		f = fopen("hello.txt", "r");
+		while(fgets(s, sizeof s, f) != NULL)
+				printf("%d: %s", ++lineN, s);
+		fclose(f);
 }
 
 int main(void){
@@ -27,7 +38,10 @@ int main(void){
 		printf("%c\n", c);
 		fclose(fp);
 
-		print_whole_line("hello.txt", fp);
+		print_whole_file("hello.txt", fp);
+
+		print_in_lines();
+
 
 		return 0;
 }

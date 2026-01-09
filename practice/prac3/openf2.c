e18763544cdf1bcc9f2cd910bed0ea90cbc80165
Sly-UbuntuWindowsTerminal <85970858+SlyNinjutsu07@users.noreply.github.com>
2026-01-08 20:32:47 -0500

    New error of just printing 1's only

--- practice/prac3/openf2.c
+++ practice/prac3/openf2.c
@@ -3,8 +3,10 @@
 int add_list(int *list, int len){
 		int sum = 0;
 
-		for(int i = 0; i < len; i++)
+		for(int i = 0; i < len; i++){
 				sum += list[i];
+				printf("%d ", list[i]);
+		}
 
 		return sum;
 }
@@ -14,15 +16,18 @@ int main(void){
 		FILE *f = fopen("numbers.bin", "rb");
 		int l[128];
 
-		while(fread(&l, sizeof(int), 1, f))
+		int count = 0;
+
+		//Prints out just 1's
+		while(count < 128 && fread(&l[count], sizeof(int), 1, f) > 0){ 
 				printf("%d ", *l);
+				count++;
+		}
 		printf("\n");
 
-		int len = sizeof(l) / sizeof(int);
+		printf("Sum: %d\n", add_list(l, 8)); 
 		
-		//Printed a stupid number
-		//Func doesnt work
-		printf("Sum: %d\n", add_list(l, len));
+
 
 		return 0;
 }
